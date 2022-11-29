f84d8d103f067e8d1895327935982cfb8c214c36
Yhago Carvalho <yhago.serafim@quintoandar.com.br>
2022-11-28 22:08:55 -0300

    consulta com vários critérios

--- src/main/java/com/example/demo/resources/PostResource.java
+++ src/main/java/com/example/demo/resources/PostResource.java
@@ -1,28 +1,18 @@
 package com.example.demo.resources;
 
 import com.example.demo.domain.Post;
-import com.example.demo.domain.User;
-import com.example.demo.dto.UserDTO;
 import com.example.demo.services.PostService;
-import com.example.demo.services.UserService;
 import com.example.demo.util.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import java.util.Date;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -43,6 +33,20 @@ public class PostResource {
         // para decodificar o texto
         text = URL.decodeParam(text);
         List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        //como após o titlesearch o critério de busca é passado com uma ? a anotação é @RequestParam
+        // para decodificar o texto
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -6,6 +6,7 @@ import com.example.demo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,11 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return postRepository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        int dayInMiliseconds = 24 * 60 * 60 * 1000;
+        maxDate = new Date(maxDate.getTime() + dayInMiliseconds);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
